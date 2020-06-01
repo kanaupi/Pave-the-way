1d9c2e261a4d08eab51174bf6193ecfc9a5c1656
kanaupi <hikari00.43.baske@icloud.com>
2020-06-02 04:32:04 +0900

    tilemapの設定とα版の仮デザインを用意。プレイヤーの基本操作と一部ギミックに対するアクションを追加しました。

---
+++ Assets/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject breakableBlock;
+    public GameObject turnleft;
+
+    bool mf=false;
+    bool mb = false;
+    bool stay = false;
+
+    public float speed = 0.5f;
+    bool buttonClicked = false;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mf)
+        {
+            Moveforward();
+        }
+        if (mb)
+        {
+            Moveback();
+        }
+        if (stay == false)
+        {
+            mf = false;
+            mb = false;
+        }
+    }
+
+    public void Moveforward()
+    {
+        player.transform.position += new Vector3(speed,0,0);
+    }
+    public void Moveback()
+    {
+        player.transform.position -= new Vector3(speed,0,0);
+    }
+    public void GoButton()
+    {
+        mf = true;
+        buttonClicked = true;
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        stay = true;
+        if (coll.gameObject.tag == "turnleft")
+        {
+            mb = true;
+            mf = false;
+        }else if (buttonClicked && coll.gameObject.tag == "BreakableBlock")
+        {
+            mf = true;
+            mb = false;
+        }
+    }
+    private void OnCollisionStay2D(Collision2D coll)
+    {
+        stay = true;
+        
+    }
+    private void OnCollisionExit2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag != "turnleft")
+        stay = false;
+    }
+
+
+}
